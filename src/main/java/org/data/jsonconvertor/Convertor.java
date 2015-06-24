b71e0b0b99740d95ac4dbd7f0dea5962a5955cbc
msivera <maud-sivera@orange.fr>
2015-06-24 11:52:47 +0200

    main and convertor adaptated for watering and imgprocprofile

--- src/main/java/org/data/jsonconvertor/Convertor.java
+++ src/main/java/org/data/jsonconvertor/Convertor.java
@@ -8,16 +8,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.data.handle.Utils;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Convertor {
-	public static List<LinkedHashMap<String, Object>> WeighingResultConvertToJson(ResultSet rs){
+	public static List<LinkedHashMap<String,Object>> WeighingResultConvertToJson(ResultSet rs){
 		List<LinkedHashMap<String,Object>> jsons = new ArrayList<LinkedHashMap<String,Object>>();
 		//JSONObject jsonObject = new JSONObject();
 		try {
 			while(rs.next()){
-				LinkedHashMap<String,Object> jsonOrderedMap = new LinkedHashMap<String,Object>();
+				LinkedHashMap<String, Object> jsonOrderedMap = new LinkedHashMap<String, Object>();
 				//dans platforme
 				jsonOrderedMap.put("plateform","http://www.phenome-fppn.fr/m3p/");
 				jsonOrderedMap.put("technicalPlateau","http://www.phenome-fppn.fr/m3p/phenoarch");
@@ -29,24 +28,20 @@ public class Convertor {
 				jsonOrderedMap.put("plant","http://www.phenome-fppn.fr/m3p/arch/2013/c13006199");
 				jsonOrderedMap.put("plantAlias","1605/22H3/ZM3597/MYB/WW/1/2745/ARCH2013-09-12");
 				
-			
 				Map<String, Object> childObject = new LinkedHashMap<String, Object>();
 				childObject.put("separator", "/");
 				childObject.put("itemsNumber", "8");
-				JSONArray objs = new JSONArray();
-				objs.add("car_number");
-				objs.add("genotype");
-				objs.add("seedlot");
-				objs.add("project");
-				objs.add("scenario");
-				objs.add("repetition");
-				objs.add("balance");
-				objs.add("experiment");
-				childObject.put("items", (Object) objs);
+				childObject.put("item1", "car_number");
+				childObject.put("item2", "genotype");
+				childObject.put("item3", "seedlot");
+				childObject.put("item4", "project");
+				childObject.put("item5", "scenario");
+				childObject.put("item6", "repetition");
+				childObject.put("item7", "balance");
+				childObject.put("item8", "experiment");
 				org.json.JSONObject childjson = new org.json.JSONObject(childObject) ;
 				//JSONArray jsonArray = new JSONArray(childjson);
 				jsonOrderedMap.put("plantPatternAlias",childjson);
-				
 				
 				//dans weighingresult
 				jsonOrderedMap.put("weighingid", Utils.convertToString(rs.getInt("weighingid")));
@@ -59,15 +54,8 @@ public class Convertor {
 				jsonOrderedMap.put("weighingtype", Utils.convertToString(rs.getString("weighingtype")));
 				jsonOrderedMap.put("reqscaletypename", Utils.convertToString(rs.getString("reqscaletypename")));
 				jsonOrderedMap.put("usedstationid", Utils.convertToString(rs.getInt("usedstationid")));
-				Map<String, Object> childObject2 = new LinkedHashMap<String, Object>();
-				childObject2.put("id", Utils.convertToString(rs.getInt("usedscaleid")));
-				childObject2.put("typename", Utils.convertToString(rs.getString("usedscaletypename")));
-				
-				org.json.JSONObject childjson2 = new org.json.JSONObject(childObject2) ;
-				//JSONArray jsonArray = new JSONArray(childjson);
-				jsonOrderedMap.put("usedscale",childjson2);
-				//jsonOrderedMap.put("usedscaleid", Utils.convertToString(rs.getInt("usedscaleid")));
-				//jsonOrderedMap.put("usedscaletypename", Utils.convertToString(rs.getString("usedscaletypename")));
+				jsonOrderedMap.put("usedscaleid", Utils.convertToString(rs.getInt("usedscaleid")));
+				jsonOrderedMap.put("usedscaletypename", Utils.convertToString(rs.getString("usedscaletypename")));
 				jsonOrderedMap.put("weightbefore", Utils.convertToString(rs.getInt("weightbefore")));
 				jsonOrderedMap.put("weightafter", Utils.convertToString(rs.getInt("weightafter")));
 				jsonOrderedMap.put("success", Utils.convertToString(rs.getBoolean("success")));
@@ -76,6 +64,117 @@ public class Convertor {
 				jsonOrderedMap.put("level", Utils.convertToString(rs.getInt("level")));
 				//jsonObject = new JSONObject(data).toString(); 
 				jsons.add(jsonOrderedMap);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsons;
+	}
+	
+	
+	public static List<LinkedHashMap<String,Object>> WateringResultConvertToJson(ResultSet rs){
+		List<LinkedHashMap<String,Object>> jsons = new ArrayList<LinkedHashMap<String,Object>>();
+		//JSONObject jsonObject = new JSONObject();
+		
+		try {
+			while(rs.next()){
+				LinkedHashMap<String, Object> jsonOrderedMap = new LinkedHashMap<String, Object>();
+				//dans platforme
+				jsonOrderedMap.put("plateform","http://www.phenome-fppn.fr/m3p/");
+				jsonOrderedMap.put("technicalPlateau","http://www.phenome-fppn.fr/m3p/phenoarch");
+				jsonOrderedMap.put("experiment","http://www.phenome-fppn.fr/m3p/ARCH2013-09-12");
+				jsonOrderedMap.put("experimentAlias","ZD13");
+				jsonOrderedMap.put("specie","maize");
+				jsonOrderedMap.put("study","");
+				jsonOrderedMap.put("studyAlias","");
+				jsonOrderedMap.put("plant","http://www.phenome-fppn.fr/m3p/arch/2013/c13006199");
+				jsonOrderedMap.put("plantAlias","1605/22H3/ZM3597/MYB/WW/1/2745/ARCH2013-09-12");
+				
+				
+				Map<String, Object> childObject = new LinkedHashMap<String, Object>();
+				childObject.put("separator", "/");
+				childObject.put("itemsNumber", "8");
+				childObject.put("item1", "car_number");
+				childObject.put("item2", "genotype");
+				childObject.put("item3", "seedlot");
+				childObject.put("item4", "project");
+				childObject.put("item5", "scenario");
+				childObject.put("item6", "repetition");
+				childObject.put("item7", "balance");
+				childObject.put("item8", "experiment");
+				org.json.JSONObject childjson = new org.json.JSONObject(childObject) ;
+				//JSONArray jsonArray = new JSONArray(childjson);
+				jsonOrderedMap.put("plantPatternAlias",childjson);
+				
+				//dans wateringresult
+				jsonOrderedMap.put("wateringId", Utils.convertToString(rs.getInt("wateringid")));
+				jsonOrderedMap.put("studyName", Utils.convertToString(rs.getString("studyname")));
+				jsonOrderedMap.put("taskId", Utils.convertToString(rs.getInt("taskid")));
+				jsonOrderedMap.put("tagName", Utils.convertToString(rs.getString("tagname")));
+				jsonOrderedMap.put("plantId", Utils.convertToString(rs.getInt("plantid")));
+				jsonOrderedMap.put("resultDate", rs.getTimestamp("resultdate").toString());
+				jsonOrderedMap.put("valid", Utils.convertToString(rs.getBoolean("valid")));
+				jsonOrderedMap.put("success", Utils.convertToString(rs.getBoolean("success")));
+				jsonOrderedMap.put("calibration", Utils.convertToString(rs.getInt("calibration")));
+
+				Map<String, Object> childRequired = new LinkedHashMap<String, Object>();
+				childRequired.put("requiredProduct", Utils.convertToString(rs.getString("reqproductname")));
+				childRequired.put("requiredScaleType", Utils.convertToString(rs.getString("reqscaletypename")));
+				childRequired.put("requiredPumpType", Utils.convertToString(rs.getString("reqpumptypename")));
+				childRequired.put("requiredTargetWeight", Utils.convertToString(rs.getInt("reqtargetweight")));
+				childRequired.put("requiredTargetQuantity", Utils.convertToString(rs.getInt("reqtargetquantity")));
+				childRequired.put("requiredPumpSpeed", Utils.convertToString(rs.getInt("reqpumpspeed")));
+				childRequired.put("requiredMaxQuantity", Utils.convertToString(rs.getInt("reqmaxquantity")));
+				childRequired.put("requiredMinWeight", Utils.convertToString(rs.getInt("reqminweight")));
+				childRequired.put("requiredMovePerch", Utils.convertToString(rs.getBoolean("reqmoveperch")));
+				childjson = new org.json.JSONObject(childRequired) ;
+				jsonOrderedMap.put("requiredConfig",childjson);
+				
+				Map<String, Object> childUsed = new LinkedHashMap<String, Object>();
+				childUsed.put("id_station", Utils.convertToString(rs.getInt("usedstationid")));
+				childUsed.put("id_usedScale", Utils.convertToString(rs.getInt("usedscaleid")));
+				childUsed.put("id_usedPump", Utils.convertToString(rs.getInt("usedpumpid")));
+				childUsed.put("usedProduct", Utils.convertToString(rs.getString("usedproductname")));
+				childUsed.put("usedScaleType", Utils.convertToString(rs.getString("usedscaletypename")));
+				childUsed.put("usedPumpType", Utils.convertToString(rs.getString("usedpumptypename")));
+				childUsed.put("usedPumpSpeed", Utils.convertToString(rs.getInt("pumpspeed")));
+				childjson = new org.json.JSONObject(childUsed) ;
+				jsonOrderedMap.put("usedConfig",childjson);
+				
+				jsonOrderedMap.put("weightBefore", Utils.convertToString(rs.getInt("weightbefore")));
+				jsonOrderedMap.put("weightAfter", Utils.convertToString(rs.getInt("weightafter")));
+				//+productAmount ??
+				
+				jsons.add(jsonOrderedMap);
+	
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsons;
+	}
+	
+	public static List<LinkedHashMap<String,Object>> ImgProcProfileConvertToJson(ResultSet rs){
+		List<LinkedHashMap<String,Object>> jsons = new ArrayList<LinkedHashMap<String,Object>>();
+		//JSONObject jsonObject = new JSONObject();
+		
+		try {
+			while(rs.next()){
+				LinkedHashMap<String, Object> jsonOrderedMap = new LinkedHashMap<String, Object>();
+				
+				//dans imgprocprofiles
+				jsonOrderedMap.put("imgProcProfileId", Utils.convertToString(rs.getInt("imgprocprofileid")));
+				jsonOrderedMap.put("imgProcProfileName", Utils.convertToString(rs.getString("imgprocprofilename")));
+				jsonOrderedMap.put("validated", Utils.convertToString(rs.getBoolean("validated")));
+				jsonOrderedMap.put("deleted", Utils.convertToString(rs.getBoolean("deleted")));
+				jsonOrderedMap.put("profileType", Utils.convertToString(rs.getInt("profiletype")));  //TODO : modif : String profileType (table profileTypes)
+				jsonOrderedMap.put("description", Utils.convertToString(rs.getString("description")));
+				jsonOrderedMap.put("imgProcScript", Utils.convertToString(rs.getString("imgprocscript")));
+			
+				jsons.add(jsonOrderedMap);
+	
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
